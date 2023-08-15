4cc4e488d645fe95edabaf45db8a8873a05a93ec
새미 <s22058@gsm.hs.kr>
2023-08-15 17:48:27 +0900

    Make ViewControllers

---
+++ ME/Controller/MyViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MyViewController.swift
+//  ME
+//
+//  Created by 새미 on 2023/08/15.
+//
+
+import UIKit
+
+final class MyViewController: UIViewController {
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ ME/Controller/ShopViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ShopViewController.swift
+//  ME
+//
+//  Created by 새미 on 2023/08/15.
+//
+
+import UIKit
+
+final class ShopViewController: UIViewController {
+
+    // MARK: - Life Cycel
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
