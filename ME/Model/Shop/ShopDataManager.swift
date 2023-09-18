fb03478e863f7c4a704a12afe79fde4c6718a30e
새미 <s22058@gsm.hs.kr>
2023-09-18 20:50:05 +0900

    Add Image

--- ME/Model/Shop/ShopDataManager.swift
+++ ME/Model/Shop/ShopDataManager.swift
@@ -26,10 +26,10 @@ final class ShopDataManager {
         shopDataArray = [
             Shop(productImage: UIImage(named: "iceBox"), brandName: "스타벅스", productName: "아이스 아메리카노 T", price: 1, notice: starbucksNotice),
             Shop(productImage: UIImage(named: "icedCafeLatte"), brandName: "스타벅스", productName: "아이스 카페 라떼 T", price: 1, notice: starbucksNotice),
-            Shop(productImage: UIImage(named: "iceBox"), brandName: "스타벅스", productName: "아이스 카라멜 마키아또 T", price: 1, notice: starbucksNotice),
+            Shop(productImage: UIImage(named: "caramelMacchiato"), brandName: "스타벅스", productName: "아이스 카라멜 마키아또 T", price: 1, notice: starbucksNotice),
             Shop(productImage: UIImage(named: "coldBrew"), brandName: "스타벅스", productName: "아이스 콜드 브루 T", price: 1, notice: starbucksNotice),
             Shop(productImage: UIImage(named: "iceBox"), brandName: "스타벅스", productName: "아이스 돌체라떼 T", price: 1, notice: starbucksNotice),
-            Shop(productImage: UIImage(named: "iceBox"), brandName: "스타벅스", productName: "자몽 허니 블랙 티 T", price: 1, notice: starbucksNotice),
+            Shop(productImage: UIImage(named: "grapefruitHoneyBlackTea"), brandName: "스타벅스", productName: "자몽 허니 블랙 티 T", price: 1, notice: starbucksNotice),
             Shop(productImage: UIImage(named: "strawberryChocolate"), brandName: "투썸플레이스", productName: "스트로베리 초콜릿 생크림", price: 1, notice: twosomeplaceNotice),
             Shop(productImage: UIImage(named: "iceBox"), brandName: "투썸플레이스", productName: "떠먹는 아이스박스", price: 1, notice: twosomeplaceNotice),
             Shop(productImage: UIImage(named: "baskinRobbinsPint"), brandName: "베스킨라빈스", productName: "파인트 아이스크림", price: 1, notice: baskinrobbinsNotice),
