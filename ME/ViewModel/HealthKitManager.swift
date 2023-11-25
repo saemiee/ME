bfdf44cabf2ee9c7677c698dbf3e379b86c6329f
새미 <s22058@gsm.hs.kr>
2023-11-25 15:39:13 +0900

    Add HealthKit Authority Request

--- ME/ViewModel/HealthKitManager.swift
+++ ME/ViewModel/HealthKitManager.swift
@@ -12,60 +12,18 @@ final class HealthKitManager {
     
     let healthStore = HKHealthStore()
     
-    let workoutTypes: Set<HKWorkoutActivityType> = [
-        .walking,
-        .swimming,
-        .cycling,
-        .pilates,
-        .running,
-        .elliptical,
-        .coreTraining,
-        .stepTraining,
-        .socialDance,
-        .cardioDance,
-        .functionalStrengthTraining,
-        .hiking,
-        .highIntensityIntervalTraining,
-        .rowing
-    ]
-    
-    var typesToRequest: Set<HKSampleType> = []
-    
-    func configure() {
-        if !HKHealthStore.isHealthDataAvailable() {
-            authorizingAccess()
-        } else {
-            print("데이터 받아오기")
-        }
-    }
-
-    func kcalCalculator() {
-        for activityType in workoutTypes {
-            let energyType = HKQuantityType.quantityType(forIdentifier: .activeEnergyBurned)!
-            let predicate = HKQuery.predicateForWorkouts(with: activityType)
-            typesToRequest.insert(energyType)
-            typesToRequest.insert(HKObjectType.workoutType())
-            
-            let query = HKStatisticsQuery(quantityType: energyType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (query, result, error) in
-                if let sum = result?.sumQuantity() {
-                    let caloriesBurned = sum.doubleValue(for: HKUnit.kilocalorie())
-                }
-            }
-            healthStore.execute(query)
-        }
-    }
-    
-    func authorizingAccess() {
-        healthStore.requestAuthorization(toShare: typesToRequest, read: typesToRequest) { (success, error) in
-            if !success {
-                if let error = error {
-                    print(error.localizedDescription)
-                } else {
-                    print("HealthKit 권한 요청 실패: 알 수 없는 오류")
-                }
+    // MARK: - Authority Request
+    func requestAuthorization() {
+        let typesToRead: Set<HKSampleType> =
+        [HKObjectType.workoutType()]
+        
+        healthStore.requestAuthorization(toShare: nil, read: typesToRead) { sucess, error in
+            if sucess {
+                self.requestAuthorization()
             } else {
-                print("HealthKit 권한 요청 성공")
+                print("error")
             }
         }
     }
+        
 }
