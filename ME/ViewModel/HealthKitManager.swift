b0907124f5044f7124375ea76dc0011cd3df3d54
새미 <s22058@gsm.hs.kr>
2023-11-28 16:00:45 +0900

    Get Workout Data

--- ME/ViewModel/HealthKitManager.swift
+++ ME/ViewModel/HealthKitManager.swift
@@ -12,10 +12,6 @@ final class HealthKitManager {
     
     let healthStore = HKHealthStore()
     
-    let workoutTypes: [HKWorkoutActivityType] = [
-        .walking, .swimming, .cycling, .pilates, .running, .elliptical, .coreTraining, .stepTraining, .socialDance, .functionalStrengthTraining, .hiking, .highIntensityIntervalTraining, .rowing
-    ]
-    
     // MARK: - Authority Request
     func requestAuthorization(completion: @escaping (Bool, Error?) -> Void) {
         guard HKHealthStore.isHealthDataAvailable() else {
@@ -29,4 +25,39 @@ final class HealthKitManager {
             completion(success, error)
         }
     }
+    
+    // MARK: - Get Workout Data
+    func getWorkoutData(workoutType: HKWorkoutActivityType, completion: @escaping (Double?, Error?) -> Void) {
+        guard let activeEnergyBurned = HKSampleType.quantityType(forIdentifier: .activeEnergyBurned) else {
+            return
+        }
+        
+        let now = Date()
+        let startDate = Calendar.current.startOfDay(for: now)
+        let endDate = Calendar.current.date(byAdding: .day, value: 1, to: startDate) ?? now
+        
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: .strictStartDate)
+        let workoutPredicate = HKQuery.predicateForWorkouts(with: workoutType)
+        
+        let compound = NSCompoundPredicate(andPredicateWithSubpredicates: [predicate, workoutPredicate])
+        
+        let query = HKStatisticsQuery(quantityType: activeEnergyBurned, quantitySamplePredicate: compound, options: .cumulativeSum) { _, result, error in
+            if let error = error {
+                completion(nil, error)
+                return
+            }
+            
+            guard let result = result, let sum = result.sumQuantity() else {
+                completion(nil, nil)
+                return
+            }
+            
+            let calories = sum.doubleValue(for: HKUnit.kilocalorie())
+            DispatchQueue.main.async{
+                completion(calories, nil)
+            }
+        }
+        healthStore.execute(query)
+    }
+
 }
